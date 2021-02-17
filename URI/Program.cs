f4dafc72dc3933165ebf23827551bbea331212f9
rafael3do <rafael3do@hotmail.com>
2021-02-16 21:17:51 -0300

    Create Solution 1000

---
+++ URI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class Program
+    {
+        static void Main_URI(string[] args)
+        {
+            //HMG
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ URI/URI_1000.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1000
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
