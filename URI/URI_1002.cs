71f2b93eb2bf396b3a0b4702d9c331f4f9d2667c
rafael3do <rafael3do@hotmail.com>
2021-02-18 19:21:47 -0300

    Solved 1002

---
+++ URI/URI_1002.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1002
+    {
+        static void Main(string[] args)
+        {
+            double n = 3.14159d;
+            double raio =double.Parse(Console.ReadLine());
+            double area = n * Math.Pow(raio, 2);
+            System.Console.WriteLine("A="+area.ToString("0.0000"));
+
+         
+        }
+
+    }
+}
