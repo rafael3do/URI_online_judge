264dfd89ee733944a29dfe0983ed19f6b68b0bde
rafael3do <rafael3do@hotmail.com>
2021-02-19 22:49:28 -0300

    Solved 1003

---
+++ URI/URI_1003.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1003
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("SOMA = " + (a + b));
+          
+        }
+
+    }
+}
