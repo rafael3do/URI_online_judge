d8415447d17a2a9f3704a6df9bae80be9735efa6
rafael3do <rafael3do@hotmail.com>
2021-02-23 19:49:04 -0300

    Solved 1005

---
+++ URI/URI_1005.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1005
+    {
+        static void Main (string [] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("MEDIA = "+ (((a*3.5)+(b*7.5))/11).ToString("F5") );
+
+            
+        }
+    }
+}
