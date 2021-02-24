09c02b2a17a4c0fbffce8c72156db9022c3ddb63
rafael3do <rafael3do@hotmail.com>
2021-02-24 20:19:50 -0300

    Solved 1006

---
+++ URI/URI_1006.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1006
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("MEDIA = " + (((a*2)+(b*3)+(c*5))/10).ToString("F1"));
+            Console.ReadKey();
+        }
+
+    }
+}
