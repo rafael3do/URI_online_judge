7414939fa0a60614b0fc2e64d4b11910736be5cc
rafael3do <rafael3do@hotmail.com>
2021-02-26 19:53:06 -0300

    Solved 1008

---
+++ URI/URI_1008.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1008
+    {
+        static void Main (string [] args)
+        {
+            int emp = Convert.ToInt32(Console.ReadLine());
+            int hour = Convert.ToInt32(Console.ReadLine());
+            double value = double.Parse(Console.ReadLine());
+            double salary = hour * value;
+            Console.WriteLine("NUMBER = " + emp);
+            Console.WriteLine("SALARY = U$ " + salary.ToString("F2"));
+
+        }
+    }
+}
