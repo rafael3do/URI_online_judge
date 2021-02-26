7d79d4d69a8b241f7a07a12963c652c91aa0107a
rafael3do <rafael3do@hotmail.com>
2021-02-25 21:09:00 -0300

    Solved 1007

---
+++ URI/URI_1007.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1007
+    {
+       static void Main (string [] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            Console.WriteLine("DIFERENCA = "+(a*b-c*d));
+            
+        }
+    }
+
+}
