f47bf31fc23009200d05d9f6e9a5064b43a1356b
rafael3do <rafael3do@hotmail.com>
2021-03-01 20:52:17 -0300

    Solved 1009

---
+++ URI/URI_1009.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1009
+    {
+
+        static void Main (string [] args)
+        {
+            string name = Console.ReadLine();
+            double salary = Convert.ToDouble(Console.ReadLine());
+            double sales = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("TOTAL = R$ " + ((sales * 0.15) + salary).ToString("F2"));
+
+        }
+    }
+}
