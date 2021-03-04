2a0b36ee8d3b06bfa200248f520ec5fba68abcf3
rafael3do <rafael3do@hotmail.com>
2021-03-04 19:59:00 -0300

    Solved 1010

---
+++ URI/URI_1010.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1010
+    {
+        static void Main(string [] args)
+        {
+
+            string linha1 = Console.ReadLine();
+            string linha2 = Console.ReadLine();
+            double total;
+
+            string[] valores = linha1.Split(' ');
+            int codPeca1 = int.Parse(valores[0]);
+            int numPecas1 = int.Parse(valores[1]);
+            double valorPeca1 = Convert.ToDouble(valores[2]);
+
+            total = numPecas1 * valorPeca1;
+
+            string[] valores2 = linha2.Split(' ');
+            int codPeca2 = int.Parse(valores2[0]);
+            int numPecas2 = int.Parse(valores2[1]);
+            double valorPeca2 = Convert.ToDouble(valores2[2]);
+
+            total += numPecas2 * valorPeca2;
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("0.00"));
+        }
+    }
+}
