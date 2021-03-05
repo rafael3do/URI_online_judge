03cf786b8b77a7c069b76147748c1b5ce314fabe
rafael3do <rafael3do@hotmail.com>
2021-03-05 20:29:17 -0300

    Solved 1011

---
+++ URI/URI_1011.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1011
+    {
+        static void  Main(string[] args)
+        {
+            double pi = 3.14159;
+            double esfera, volume;
+            esfera = double.Parse(Console.ReadLine());
+            volume = (4 / 3.0) * pi * (Math.Pow(esfera, 3));
+            Console.WriteLine("VOLUME = " + volume.ToString("0.000"));
+        }
+    }
+}
