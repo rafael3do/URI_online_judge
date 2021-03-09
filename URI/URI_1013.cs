b6aebd1a4d274d49694c3baaf5e96a28be2abc7d
rafael3do <rafael3do@hotmail.com>
2021-03-09 20:23:51 -0300

    Solved 1013

---
+++ URI/URI_1013.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1013
+    {
+        static void Main (string[] args)
+        {
+            int a, b, c, MaiorAB, MaiorABC;
+            string linha = Console.ReadLine();
+            String[] valores = linha.Split(' ');
+            a = int.Parse(valores[0]);
+            b = int.Parse(valores[1]);
+            c = int.Parse(valores[2]);
+            MaiorAB = (a + b + Math.Abs(a - b)) / 2;
+            MaiorABC = (MaiorAB + c + Math.Abs(MaiorAB - c)) / 2;
+            Console.WriteLine(MaiorABC + " eh o maior");
+        }
+    }
+}
