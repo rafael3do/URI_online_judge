244b1a4120cdcf846578ff12fbf4dda2ec4517cd
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-10 20:47:33 -0300

    Solved 1014

---
+++ URI/URI_1014.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1014
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine((x/y).ToString("F3")+" km/l");
+
+        }
+    }
+}
