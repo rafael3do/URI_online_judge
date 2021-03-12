8e193a210d3c109fe7c6702bb1a7563847668a1f
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-12 20:35:48 -0300

    Solved 1016

---
+++ URI/URI_1016.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1016
+    {
+        static void Main(string [] args)
+        {
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine(y*2+" minutos");
+         
+        }
+    }
+}
