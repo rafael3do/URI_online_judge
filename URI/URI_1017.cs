e8fb0526bb9ee332121bda65195abd66637e056d
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-15 22:00:30 -0300

    Solved 1017

---
+++ URI/URI_1017.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1017
+    {
+        static void Main(string [] args)
+        {
+            float horas, km,distancia;            
+            horas = float.Parse(Console.ReadLine());
+            km = float.Parse(Console.ReadLine());
+            distancia = ((km * horas) / 12);
+            Console.WriteLine(distancia.ToString("N3"));
+        }
+
+    }
+}
