391eabbf2e08b2d3b12173a537d1824d4ecd5963
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-18 19:39:20 -0300

    Solved URI 1020

---
+++ URI/URI_1020.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1020
+    {
+        static void Main (string [] args)
+        {
+            int valor, ano, mes, dias;
+            valor = Convert.ToInt32(Console.ReadLine());
+            ano = valor / 365;
+            valor = valor - (ano * 365);
+            mes = valor / 30;
+            valor = valor - (mes * 30);
+            dias = valor;
+            Console.WriteLine(ano + " ano(s)\n" + mes + " mes(es)\n" + dias + " dia(s)");
+        }
+    }
+}
