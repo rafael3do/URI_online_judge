c9e2dda98959cbbea365973eadd58e7e053b456c
Rafael Figueiredo <rafael3do@hotmail.com>
2021-03-17 21:24:48 -0300

    Solved 1019

--- URI/AssemblyInfo.cs
+++ URI/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
 [assembly: AssemblyTitle("URI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("prjects")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("rafael3do")]
 [assembly: AssemblyProduct("URI")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright rafael3do ©  2021")]
+[assembly: AssemblyTrademark("rafael3do")]
 [assembly: AssemblyCulture("")]
 
 // Definir ComVisible como false torna os tipos neste assembly invisíveis
---
+++ URI/URI_1019.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1019
+    {
+        static void Main (string [] args)
+        {
+            int N, hora, minuto, segundo;
+            N = Convert.ToInt32(Console.ReadLine());
+            hora = N / 3600;
+            N = N - (hora * 3600);
+            minuto = N / 60;
+            N = N - (minuto * 60);
+            segundo = N;
+            Console.WriteLine(hora + ":" + minuto + ":" + segundo);            
+        }
+    }
+}
