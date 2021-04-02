3571e6bddf76b2625856086d88399f58f1f7cabe
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-02 20:38:00 -0300

    Solved 1044

---
+++ URI/URI_1044.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1044
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            string valores = Console.ReadLine();
+            int[] valoresfloat = valores.Split(' ').Select(x => int.Parse(x)).ToArray();
+            a = valoresfloat[0];
+            b = valoresfloat[1];
+            if (a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
