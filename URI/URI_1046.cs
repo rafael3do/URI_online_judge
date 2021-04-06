4a268f5e6b1c1c2311cda36360506de474a6007f
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-06 20:15:40 -0300

    Solved 1046

---
+++ URI/URI_1046.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1046
+    {
+        static void Main (string [] args)
+        {
+            int horaini, horafim, duracao;
+            string valores = Console.ReadLine();
+            int[] valoresint = valores.Split(' ').Select(x => int.Parse(x)).ToArray();
+            horaini = valoresint[0];
+            horafim = valoresint[1];
+            duracao = horaini - horafim;
+            if (horaini == horafim)
+            {
+                duracao = 24 - duracao;
+                Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+            }
+            else if (horaini <= horafim)
+            {
+                duracao = horafim - horaini;
+                Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+            }
+            else
+            {
+                duracao = 24 - duracao;
+                Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+            }
+        }
+    }
+}
