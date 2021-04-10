084bda7e8e074554bdf6821b59cb51edb72bf4eb
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-09 21:02:37 -0300

    Solved URI 1049

---
+++ URI/URI_1049.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+namespace URI
+{
+    class URI_1049
+    {
+        static void Main(string[] args)
+        {
+            string escolha1, escolha2, escolha3;
+            escolha1 = Console.ReadLine();
+            escolha2 = Console.ReadLine();
+            escolha3 = Console.ReadLine();
+
+            if (escolha1 == "vertebrado")
+            {
+                if (escolha2 == "ave")
+                {
+                    if (escolha3 == "carnivoro")
+                    {
+                        Console.WriteLine("aguia");
+                    }
+                    else if (escolha3 == "onivoro")
+                    {
+                        Console.WriteLine("pomba");
+                    }
+                }
+
+                if (escolha2 == "mamifero")
+                {
+                    if (escolha3 == "onivoro")
+                    {
+                        Console.WriteLine("homem");
+                    }
+                    else if (escolha3 == "herbivoro")
+                    {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+
+            if (escolha1 == "invertebrado")
+            {
+                if (escolha2 == "inseto")
+                {
+                    if (escolha3 == "hematofago")
+                    {
+                        Console.WriteLine("pulga");
+                    }
+                    else if (escolha3 == "herbivoro")
+                    {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                if (escolha2 == "anelideo")
+                {
+                    if (escolha3 == "hematofago")
+                    {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else if (escolha3 == "onivoro")
+                    {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+
+        }
+    }
+}
