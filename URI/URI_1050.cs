fa7a7a4c9619d895b4c5c747d3e17ba769e72915
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-12 20:59:49 -0300

    Solved URI 1050

---
+++ URI/URI_1050.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1050
+    {
+        static void Main (string [] args)
+        {
+            int ddd;
+            ddd = int.Parse(Console.ReadLine());
+            switch (ddd)
+            {
+                case 11:
+                    Console.WriteLine("Sao Paulo");
+                    break;
+                case 19:
+                    Console.WriteLine("Campinas");
+                    break;
+                case 21:
+                    Console.WriteLine("Rio de Janeiro");
+                    break;
+                case 27:
+                    Console.WriteLine("Vitoria");
+                    break;
+                case 31:
+                    Console.WriteLine("Belo Horizonte");
+                    break;
+                case 32:
+                    Console.WriteLine("Juiz de Fora");
+                    break;
+                case 61:
+                    Console.WriteLine("Brasilia");
+                    break;
+                case 71:
+                    Console.WriteLine("Salvador");
+                    break;
+                default:
+                    Console.WriteLine("DDD nao cadastrado");
+                    break;
+            }
+        }
+    }
+}
