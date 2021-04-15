efff19568110af33fb836236ba58ebe4698f0a67
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-15 20:12:01 -0300

    Solved URI 1059

---
+++ URI/URI_1059.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1059
+    {
+        static void Main(string[] args){
+            for (int i = 2; i <= 100; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
