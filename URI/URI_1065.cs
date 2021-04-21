26531e542fd369974fa9c203a347325709d482c2
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-21 18:50:42 -0300

    Solved URI 1065

---
+++ URI/URI_1065.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1065
+    {
+        static void Main (string [] args)
+        {
+            float n1;
+            int nos = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                n1 = float.Parse(Console.ReadLine());
+                if (n1 % 2 == 0)
+                {
+                    nos += 1;
+                }
+            }
+            Console.WriteLine(nos + " valores pares");
+        
+    }
+    }
+}
