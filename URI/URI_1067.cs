a6bd0360fd969d85811b429dd384e072112c6ba7
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-23 19:57:09 -0300

    Solved URI 1066

---
+++ URI/URI_1067.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1067
+    {
+        static void Main (string [] args)
+        {
+            int numero = 0;
+            numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= numero; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
+    }
+}
