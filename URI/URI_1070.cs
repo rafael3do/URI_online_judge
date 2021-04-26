a719bb52f7facb1080e00f3f2e972a71afad1cfa
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-26 19:26:26 -0300

    Solved URI 1070

---
+++ URI/URI_1070.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1070
+    {
+        static void Main (string [] args)
+        {
+            int numero = Convert.ToInt32(Console.ReadLine());
+            int controle = 0;
+
+            for (int i = numero; controle < 6; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    controle++;
+                }
+
+
+            }
+        }
+    }
+}
