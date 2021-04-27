1cfa1b2a820b86618a66df5a82993556de43a4d2
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-27 20:19:56 -0300

    Solved URI 1071

---
+++ URI/URI_1071.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1071
+    {
+        static void Main (string [] args)
+        {
+           
+            int z = 0;
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            if (x == y)
+            {
+                Console.WriteLine(z.ToString());
+            }
+            else if (x < y)
+            {
+                for (x = x + 1; x < y; x++)
+                {
+                    if (x % 2 == 1 || x % 2 == -1)
+                    {
+                        z += x;
+                    }
+
+                }
+                Console.WriteLine(z.ToString());
+            }
+            else if (x > y)
+            {
+                for (y = y + 1; y < x; y++)
+                {
+                    if (y % 2 == 1 || y % 2 == -1)
+                    {
+                        z += y;
+                    }
+                }
+                Console.WriteLine(z.ToString());
+            }
+        }
+    }
+}
