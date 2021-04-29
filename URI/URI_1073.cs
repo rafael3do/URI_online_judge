bd1d0e1423623a105c333d48766b68c1b76c3b22
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-29 19:52:07 -0300

    Solved URI 1073

---
+++ URI/URI_1073.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1073
+    {
+        static void Main(string [] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + "^" + 2 + " = " + Math.Pow(i, 2));
+                }
+            }
+        }
+    }
+}
