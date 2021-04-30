c8da298fd874590e6bcf1be3472deec966f0193d
Rafael Figueiredo <rafael3do@hotmail.com>
2021-04-30 20:33:58 -0300

    Solved URI 1074

---
+++ URI/URI_1073.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace URI
+{
+    class URI_1073
+    {
+        static void Main (string [] args)
+        {
+            int ciclos = Convert.ToInt32(Console.ReadLine());
+            int[] numeros = new int[ciclos];
+            for (int i = 0; i < ciclos; i++)
+            {
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+
+                if (numeros[i] < 0 && numeros[i] % 2 == 0)
+                {
+                    Console.WriteLine("EVEN NEGATIVE");
+                }
+                else if (numeros[i] > 0 && numeros[i] % 2 == 0)
+                {
+                    Console.WriteLine("EVEN POSITIVE");
+                }
+                else if (numeros[i] > 0 && numeros[i] % 2 != 0)
+                {
+                    Console.WriteLine("ODD POSITIVE");
+                }
+                else if (numeros[i] < 0 && numeros[i] % 2 != 0)
+                {
+                    Console.WriteLine("ODD NEGATIVE");
+                }
+                else if (numeros[i] == 0)
+                {
+                    Console.WriteLine("NULL");
+                }
+            }
+        }
+    }
+}
