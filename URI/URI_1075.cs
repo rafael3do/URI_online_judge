5263baa526338ad5ea950275d4e9ea7bb4d92e29
Rafael Figueiredo <rafael3do@hotmail.com>
2021-05-03 21:33:08 -0300

    Solved URI 1075

---
+++ URI/URI_1075.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1075
+    {
+        static void Main(string[] args)
+        {
+            int numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (i % numero == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
