2a43599bc23663c888ae39d204df90d3a8b5c304
Rafael Figueiredo <rafael3do@hotmail.com>
2021-05-06 20:02:19 -0300

    Solved URI 1080

---
+++ URI/URI_1080.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace URI
+{
+    class URI_1080
+    {
+        static void Main (string[] args)
+        {
+            int maior = 0, posicao = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (maior > num)
+                {
+                    maior = maior;
+                    posicao = posicao;
+                }
+                else
+                {
+                    posicao = i;
+                    maior = num;
+                }
+
+            }
+           Console.Write(maior + "\n" + posicao + "\n");
+           
+        }
+    
+    }
+}
